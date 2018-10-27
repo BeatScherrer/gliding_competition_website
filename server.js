239fadd1ed4e682a5f63d892dedb06c952af21b1
Beat Scherrer <beat.scherrer@gmail.com>
2018-10-27 15:11:42 +0200

    changed server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const port = process.env.PORT || 5000;
-const app = express();
-
-app.use(express.static(__dirname + "/build/"));
-app.get(/.*/, (req, res)=> {
-  res.sendFile(__dirname + "/index.html");
-});
-
+// server.js
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
 app.listen(port);
-
-console.log("Server listening on port " + port + "...");
+console.log('server started '+ port);
