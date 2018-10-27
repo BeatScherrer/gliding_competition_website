69931a954fd3c4bfc4c1bed8685604158a6acd48
Beat Scherrer <beat.scherrer@gmail.com>
2018-10-27 18:09:07 +0200

    tweaks for local development

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
 
 app.get(/.*/, (req, res)=> {
   res.sendFile(__dirname + "/index.html");
