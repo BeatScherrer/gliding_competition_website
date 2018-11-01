097b9a063e6b25d53e69020dc62219f0acb6c768
Beat Scherrer <beat.scherrer@gmail.com>
2018-11-01 17:30:58 +0100

    changed port to 5000 (heroku requirement)

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get(/.*/, (req, res)=> {
   res.sendFile(__dirname + "/dist/index.html");
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log(`Server is listening on port ${port}...`);
