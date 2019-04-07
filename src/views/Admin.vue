b046e1576c0d95a4413a10c8249bb3ec3f85bf6d
Beat Scherrer <beat.scherrer@gmail.com>
2019-04-07 14:53:29 +0200

    changed admin interface to only show verified members

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -131,7 +131,7 @@ export default {
     const vm = this;
 
     // Get all documents with verified property is true.
-    firebase.firestore().collection("users").get()
+    firebase.firestore().collection("users").where("verified", "==", true).get()
     .then(function(querySnapshot) {
       querySnapshot.forEach(function(doc) {
         // set all verified participants to the users array.
