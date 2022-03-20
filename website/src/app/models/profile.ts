371f092f01fbed856bd18c40d23cf4fe65840dfe
Beat Scherrer <b.scherrer@mt-robot.com>
2022-03-20 21:33:10 +0100

    wip on profile component

---
+++ website/src/app/models/profile.ts
@@ -0,0 +1,10 @@
+export interface Profile {
+  // personal data
+  firstName: string;
+  lastName: string;
+  email: string;
+
+  // gliding data
+  glidingGroup: string;
+  glider: string;
+}
--- website/src/app/profile/profile.component.ts
+++ website/src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Profile } from '@models/profile';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  vm$!: Subject<Profile>;
+
+  constructor(private firestoreService: FirestoreService) {
+    this.vm$ = this.firestoreService.getProfile()
+  }
 
   ngOnInit(): void {
   }
